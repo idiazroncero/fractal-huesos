5a4442a9c24b1b0f24a8fbf0b6a2a247850d5d98
Nacho Diaz <idiaz.roncero@gmail.com>
2019-02-13 20:35:15 +0000

    Initial config

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 'use strict';
 
+const mandelbrot = require('@frctl/mandelbrot');
+
+/*
+ * Configure the theme
+ */
+const huesos = mandelbrot({
+    styles: [
+        'default', 
+        '/assets/huesos.css'
+    ] // link to the default stylesheet followed by a custom one
+});
+
+/*
+ * Specify a template directory to override any view templates
+ */
+huesos.addLoadPath(__dirname + '/views');
+
+/*
+ * Specify the static assets directory that contains the custom stylesheet.
+ */
+huesos.addStatic(__dirname + '/assets');
+
+/*
+ * Export the customised theme instance so it can be used in Fractal projects
+ */
+module.exports = huesos;
+
 module.exports = require('./src/theme');
