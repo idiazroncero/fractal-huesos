1b6234757567304f3393385321a799c2e1609f47
Nacho Diaz <idiaz.roncero@gmail.com>
2019-02-25 18:01:29 +0000

    Exporta CSS temporalmente al directorio padre + unifica todo el CSS en un tema coherente sobre el que trabajar

--- gulpfile.js
+++ gulpfile.js
@@ -21,12 +21,26 @@ gulp.task('css', function() {
       .pipe(gulp.dest('./assets/'));
   });
 
+  // TEMPORARY TASK FOR TESTING THEME
+  gulp.task('css2', function() {
+    return gulp.src('./assets/scss/huesos.scss')
+      .pipe(sassGlob())
+      .pipe(sass({
+          includePaths: 'node_modules'
+      }).on('error', sass.logError))
+      .pipe(autoprefixer({
+          browsers: ['last 5 versions']
+      }))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('../../dist/'));
+  });
+
 gulp.task('css:clean', function() {
     return del(['./assets/huesos.css']);
 });
 
 gulp.task('css:watch', function () {
-    gulp.watch('./assets/scss/**/*.scss', ['css']);
+    gulp.watch('./assets/scss/**/*.scss', ['css2']);
 });
 
 gulp.task('default', ['css']);
